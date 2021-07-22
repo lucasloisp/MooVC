d58f8b306655f6ff9b186fe62b72b81b85ac62a4
Lucas Lois <lucasloisp@gmail.com>
2021-07-21 22:24:15 -0300

    feat: implements the loginviewcontroller

---
+++ MoViesC/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MoViesC
+//
+//  Created by Lucas Lois on 21/7/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+}
+
+extension LoginViewController : UITextFieldDelegate {
+    private func formIsFilledIn() -> Bool {
+        guard let username = usernameTextField.text,
+              let password = passwordTextField.text else {
+            return false
+        }
+
+        return !username.isEmpty && !password.isEmpty
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        signInButton.isEnabled = formIsFilledIn()
+        return true
+    }
+}
--- MoViesCTests/LoginViewControllerTests.swift
+++ MoViesCTests/LoginViewControllerTests.swift
@@ -6,27 +6,49 @@
 //
 
 import XCTest
+@testable import MoViesC
 
 class LoginViewControllerTests: XCTestCase {
-
+    
+    var viewController: LoginViewController!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        _ = viewController.view
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSignInButtonIsDisabledWithEmptyInputs() {
+        XCTAssertFalse(viewController.signInButton.isEnabled)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSignInButtonIsEnabledWhenBothFieldsHaveAValue() {
+        typeInTheUsername()
+        typeInThePassword()
+        XCTAssertTrue(viewController.signInButton.isEnabled)
     }
-
+    
+    private func typeInTheUsername() {
+        let usernameField = viewController.usernameTextField!
+        typeIntoField(usernameField)
+    }
+    
+    private func typeInThePassword() {
+        let passwordField = viewController.passwordTextField!
+        typeIntoField(passwordField)
+    }
+    
+    fileprivate func typeIntoField(_ field: UITextField) {
+        field.text = "a"
+        let _ =  field.delegate?.textField!(
+            field,
+            shouldChangeCharactersIn: NSMakeRange(0, 1),
+            replacementString: "a"
+        )
+    }
+    
 }
