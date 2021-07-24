8af372ebbc0e450c6a43c65f61363a9e99f37844
Lucas Lois <lucasloisp@gmail.com>
2021-07-24 13:54:09 -0300

    feat: extracts GenreMoviesManager to another file

--- MoViesC/DiscoverViewController.swift
+++ MoViesC/DiscoverViewController.swift
@@ -7,42 +7,6 @@
 
 import UIKit
 
-class GenreMoviesManager {
-    static let shared = GenreMoviesManager()
-    private init() {}
-
-    func loadGenres(onSuccess: @escaping (([(Genre, [Movie])]) -> Void), onError: (() -> Void)) {
-        APIClient.shared.requestItem(request: MovieDBRoute.getGenres) { (result: Result<GenresResponse, Error>) in
-            switch result {
-            case .success(let genresResponse):
-                let genres = genresResponse.genres
-                var movies: [[Movie]] = Array(repeating: [], count: genres.count)
-                let group = DispatchGroup()
-                genres.enumerated().forEach { (index, genre) in
-                    group.enter()
-                    let request = MovieDBRoute.discoverMoviesByGenre(genre: genre)
-                    APIClient.shared.requestItem(request: request) { (result: Result<DiscoverMovieResponse, Error>) in
-                        switch result {
-                        case .success(let response):
-                            movies[index] = response.movies
-                        case .failure(let err):
-                            // TODO: Show the error to the user
-                            print(err)
-                        }
-                        group.leave()
-                    }
-                }
-                group.notify(queue: .main) {
-                  onSuccess(Array(zip(genres, movies)))
-                }
-            case .failure(let err):
-                onError()
-            }
-        }
-
-    }
-}
-
 class DiscoverViewController: UIViewController {
     private var genreMoviesControllers: [GenreMoviesCollectionViewController]?
 
