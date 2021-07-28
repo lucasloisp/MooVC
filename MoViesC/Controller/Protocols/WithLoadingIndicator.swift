079d659bc3f9588874e085beef51a53c4f2858fd
Lucas Lois <lucasloisp@gmail.com>
2021-07-27 21:53:05 -0300

    feat: hides the collection view while loading the search results

--- MoViesC/Controller/Protocols/WithLoadingIndicator.swift
+++ MoViesC/Controller/Protocols/WithLoadingIndicator.swift
@@ -9,15 +9,20 @@ import UIKit
 
 protocol WithLoadingIndicator {
     var activityIndicatorView: UIActivityIndicatorView! { get }
+    var viewThatHidesOnLoading: UIView? { get }
 }
 
 extension WithLoadingIndicator {
+    var viewThatHidesOnLoading: UIView? { return nil }
+
     func startLoadingIndicator() {
+        viewThatHidesOnLoading?.isHidden = true
         activityIndicatorView.isHidden = false
         activityIndicatorView.startAnimating()
     }
 
     func stopLoadingIndicator() {
+        viewThatHidesOnLoading?.isHidden = false
         activityIndicatorView.isHidden = true
         activityIndicatorView.stopAnimating()
     }
