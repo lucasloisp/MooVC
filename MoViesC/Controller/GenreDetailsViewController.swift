aee577db309ef86d35661fbe5419e1fca5b8b758
Lucas Lois <lucasloisp@gmail.com>
2021-08-01 15:38:20 -0300

    refactor: extracts common InfiniteMovieListingControllerDelegate implementation

--- MoViesC/Controller/GenreDetailsViewController.swift
+++ MoViesC/Controller/GenreDetailsViewController.swift
@@ -72,28 +72,7 @@ class GenreDetailsViewController: UIViewController, WithLoadingIndicator, WithSe
 
 }
 
-extension GenreDetailsViewController: InfiniteMovieListingControllerDelegate {
-    func onFetchSucceeded(for indexes: [Int]?) {
-        guard let indexes = indexes else {
-            stopLoadingIndicator()
-            moviesCollectionView.reloadData()
-            return
-        }
-        let newIndexPathsToReload = indexes.map { IndexPath(row: $0, section: 0) }
-        let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
-        moviesCollectionView.reloadItems(at: indexPathsToReload)
-    }
-
-    private func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
-        let indexPathsForVisibleRows = moviesCollectionView.indexPathsForVisibleItems
-        let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
-        return Array(indexPathsIntersection)
-    }
-
-    func onFetchFailed() {
-        // TODO: Indicate the error to the user
-    }
-}
+extension GenreDetailsViewController: InfiniteMovieListingControllerDelegate {}
 
 extension GenreDetailsViewController: MovieListingControllerDelegate {
     func didSelect(movie: Movie) {
--- MoViesC/Controller/MovieListing/InfiniteMovieListingController.swift
+++ MoViesC/Controller/MovieListing/InfiniteMovieListingController.swift
@@ -7,9 +7,31 @@
 
 import UIKit
 
-protocol InfiniteMovieListingControllerDelegate: AnyObject {
-    func onFetchSucceeded(for indexes: [Int]?)
-    func onFetchFailed()
+protocol InfiniteMovieListingControllerDelegate: WithLoadingIndicator, AnyObject {
+    var moviesCollectionView: UICollectionView! { get }
+}
+
+extension InfiniteMovieListingControllerDelegate {
+    func onFetchSucceeded(for indexes: [Int]?) {
+        guard let indexes = indexes else {
+            stopLoadingIndicator()
+            moviesCollectionView.reloadData()
+            return
+        }
+        let newIndexPathsToReload = indexes.map { IndexPath(row: $0, section: 0) }
+        let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
+        moviesCollectionView.reloadItems(at: indexPathsToReload)
+    }
+
+    private func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
+        let indexPathsForVisibleRows = moviesCollectionView.indexPathsForVisibleItems
+        let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
+        return Array(indexPathsIntersection)
+    }
+
+    func onFetchFailed() {
+        // TODO: Indicate the error to the user
+    }
 }
 
 class InfiniteMovieListingController: MovieListingController {
