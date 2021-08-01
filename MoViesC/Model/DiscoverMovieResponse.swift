a76e965703e92429ff49086ffe55a52334811327
Lucas Lois <lucasloisp@gmail.com>
2021-08-01 10:21:46 -0300

    feat: tracks total results on the DiscoverMovieResponse page

--- MoViesC/Model/DiscoverMovieResponse.swift
+++ MoViesC/Model/DiscoverMovieResponse.swift
@@ -11,11 +11,13 @@ import ObjectMapper
 struct DiscoverMovieResponse {
     let page: Int
     let movies: [Movie]
+    let totalResults: Int
 }
 
 extension DiscoverMovieResponse: ImmutableMappable {
     init(map: Map) throws {
         page = try map.value("page")
         movies = try map.value("results")
+        totalResults = try map.value("total_results")
     }
 }
